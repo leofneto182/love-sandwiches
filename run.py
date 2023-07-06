4ed4507f1edb42b156229e9b01e37152ba3f95cc
Leonidio Neto <leonidio.freitas@outlook.com>
2023-07-06 21:51:32 +0100

    Create get_sales_data function to get user data

--- run.py
+++ run.py
@@ -12,8 +12,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    """
+    Get sales figures inut from the server
+    """
+    print('Please enter sales data from the last maket.')
+    print('Data should be six number, separated by commas.')
+    print('Example: 10,20,30,40,50,60\n')
 
-data = sales.get_all_values()
+    data_str = input('Enter yout data here: ')
+    print(f"The data provided is: {data_str}")
 
-print(data)
+get_sales_data()

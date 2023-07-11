2c60aec637be0eda78318dd31af1877c59e2f38d
Leonidio Neto <leonidio.freitas@outlook.com>
2023-07-11 23:23:59 +0100

    Collect sales data for last 5 markets in list of columns

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    Get sales figures inut from the server
+    Get sales figures input from the server
     """
 
     while True:
@@ -85,14 +85,32 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Collects collumns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists
+    """
+    sales = SHEET.worksheet('sales')
+    # column = sales.col_values(3)
+    # print(column)
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
 def main():
     """
     Run all program functions
     """
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_worksheet(sales_data, 'sales')
-new_surplus_data = calculate_surplus_data(sales_data)
-update_worksheet(new_surplus_data, 'surplus')
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_worksheet(sales_data, 'sales')
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, 'surplus')
 
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
